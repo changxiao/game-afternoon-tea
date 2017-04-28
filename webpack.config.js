984635e03a61a788b9fce570e7effe69a4685e4e
changxiao <changxiao0707@sina.com>
2017-04-28 15:39:18 +0800

    updata

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-	devtool: 'eval-source-map',
-	entry: __dirname + '/app/main.js',
+	devtool: false,
+	// devtool: 'eval-source-map',
+	entry: {
+		main: __dirname + '/app/main.js',
+		vendor: ['react']
+	},
 	output: {
 		path: __dirname + '/public',
-		filename: 'bundle.js'
+		publicPath: '/public',
+		filename: '[name]-[hash].js'
 	},
 
 	module: {
@@ -19,11 +28,24 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				loader: 'style-loader!css-loader'
+				loader: 'style-loader!css-loader?modules!postcss-loader'
 			}
 		]
 	},
-
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin(),
+	  	new webpack.LoaderOptionsPlugin({
+		  	options:{
+		  		postcss: function(){
+					return [autoprefixer]
+		  		}
+		  	}
+		}),
+	  	new webpack.BannerPlugin('Copyright Flying Unicorns inc.'),
+	  	new HtmlWebpackPlugin({
+	      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+	    })
+	],
 	devServer: {
 		contentBase: './public',
 		port: 8080,
